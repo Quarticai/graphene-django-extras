4cdecb8f5579cf7a0abfbe673c2336af571796fd
Sourcery AI <>
2021-10-12 09:36:28 +0000

    'Refactored by Sourcery'

--- graphene_django_extras/mutation.py
+++ graphene_django_extras/mutation.py
@@ -80,11 +80,7 @@ class DjangoSerializerMutation(ObjectType):
                         "https://github.com/graphql-python/graphene/blob/2.0/UPGRADE-v2.0.md#mutation-input"
                     ).format(name=cls.__name__)
                 )
-        if input_class:
-            arguments = props(input_class)
-        else:
-            arguments = {}
-
+        arguments = props(input_class) if input_class else {}
         registry = get_global_registry()
 
         factory_kwargs = {
@@ -176,10 +172,8 @@ class DjangoSerializerMutation(ObjectType):
             for field in cls._meta.nested_fields:
                 sub_data = data.pop(field, None)
                 if sub_data:
-                    serialized_data = cls._meta.nested_fields[field](
-                        data=sub_data, many=True if type(
-                            sub_data) == list else False
-                    )
+                    serialized_data = cls._meta.nested_fields[field](data=sub_data, many=type(
+                            sub_data) == list)
                     ok, result = cls.save(serialized_data, root, info)
                     if not ok:
                         return cls.get_errors(result)
@@ -248,12 +242,14 @@ class DjangoSerializerMutation(ObjectType):
     @classmethod
     def update(cls, root, info, **kwargs):
         data = kwargs.get(cls._meta.input_field_name['update'])
-        m2m_dict = {}
         m2m_fields = [
             field.attname for field in cls._meta.model._meta.local_many_to_many]
-        for m2m_field in m2m_fields:
-            if data.get(m2m_field):
-                m2m_dict[m2m_field] = data.pop(m2m_field)
+        m2m_dict = {
+            m2m_field: data.pop(m2m_field)
+            for m2m_field in m2m_fields
+            if data.get(m2m_field)
+        }
+
         request_type = info.context.META.get("CONTENT_TYPE", "")
         if "multipart/form-data" in request_type:
             data.update(
