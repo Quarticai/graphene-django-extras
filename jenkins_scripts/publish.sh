86dfe4d94e2a1ed892c71a28332cf827230d1860
mayuresh <mayuresh@quartic.ai>
2024-07-09 16:44:53 +0530

    QL-646: add uv and update requirements

--- jenkins_scripts/publish.sh
+++ jenkins_scripts/publish.sh
@@ -8,5 +8,7 @@ echo $PWD
 set -ex
 echo "$BRANCH_NAME"
 
-pip install -r requirements.txt
+pip install -U pip==$PIP_VERSION
+pip install uv==$UV_VERSION
 
+uv pip install -r requirements.txt --system -f https://download.pytorch.org/whl/torch_stable.html
