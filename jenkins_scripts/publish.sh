a4aab1373e3ff8b5141b22e71e3cbc2ffc983e2a
Mayuresh <104883828+mayuresh-quartic@users.noreply.github.com>
2024-07-10 09:20:10 +0530

    QL-646: add uv and update requirements and cleanup (#109)
    * QL-646: add uv and update requirements
    
    * QL-646: add links

--- jenkins_scripts/publish.sh
+++ jenkins_scripts/publish.sh
@@ -8,5 +8,7 @@ echo $PWD
 set -ex
 echo "$BRANCH_NAME"
 
-pip install -r requirements.txt
+pip install -U pip==$PIP_VERSION
+pip install uv==$UV_VERSION
 
+uv pip install -r requirements.txt --system --extra-index-url https://pypi.fury.io/cofDNgE692FsjZ5iiShW/quartic-ai/ -f https://download.pytorch.org/whl/torch_stable.html
